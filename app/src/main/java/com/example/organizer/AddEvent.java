c9a0a926be01248a0d71be46e6965b9814e94a0b
sasa-bonta <bonta2000@mail.ru>
2020-10-16 16:03:48 +0300

    notif

--- app/src/main/java/com/example/organizer/AddEvent.java
+++ app/src/main/java/com/example/organizer/AddEvent.java
@@ -3,8 +3,12 @@ package com.example.organizer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -20,8 +24,13 @@ import com.example.organizer.SQL.AppDatabase;
 import com.example.organizer.SQL.Event;
 import com.example.organizer.SQL.MainAdapter;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class AddEvent extends AppCompatActivity {
@@ -29,6 +38,19 @@ public class AddEvent extends AppCompatActivity {
     private static final String TAG = "AddEvent";
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TimePickerDialog.OnTimeSetListener mTimeSetListener;
+
+    final Calendar cal = Calendar.getInstance();
+    final Calendar current = Calendar.getInstance();
+
+    int[] year = new int[1];
+    int[] month = new int[1];
+    int[] day = new int[1];
+    int[] hour = new int[1];
+    int[] minute = new int[1];
+
+    final static int RQS_1 = 1;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +63,25 @@ public class AddEvent extends AppCompatActivity {
 
         database = AppDatabase.getInstance(this);
         eventList = database.eventDao().getAll();
+        context = getApplicationContext();
 
         addEvent();
 
         final TextView mDisplayDate = findViewById(R.id.date);
-        final TextView mDisplaTime = findViewById(R.id.time);
+        final TextView mDisplayTime = findViewById(R.id.time);
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
+            //    Calendar cal = Calendar.getInstance();
+                year[0] = cal.get(Calendar.YEAR);
+                month[0] = cal.get(Calendar.MONTH);
+                day[0] = cal.get(Calendar.DAY_OF_MONTH);
 
                 DatePickerDialog dialog = new DatePickerDialog(AddEvent.this,
                         android.R.style.Theme_Black,
                         mDateSetListener,
-                        year, month, day);
+                        year[0], month[0], day[0]);
 
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
@@ -68,25 +91,32 @@ public class AddEvent extends AppCompatActivity {
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month +=1;
+
+                cal.set(Calendar.DAY_OF_MONTH, day);
+                cal.set(Calendar.MONTH, month);
+                cal.set(Calendar.YEAR, year);
+
+                month+=1;
                 Log.d(TAG, "onDateSet : dd/mm/yyyy: "  + day + "/" + month + "/" + year);
 
                 String date = day + "/" + month + "/" + year;
-                mDisplayDate.setText("Date: " + date);
+                mDisplayDate.setText(date);
+
+
             }
         };
 
-        mDisplaTime.setOnClickListener(new View.OnClickListener() {
+        mDisplayTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int hour = cal.get(Calendar.HOUR_OF_DAY);
-                final int minute = cal.get(Calendar.MINUTE);
+            //    Calendar cal = Calendar.getInstance();
+                hour[0] = cal.get(Calendar.HOUR_OF_DAY);
+                minute[0] = cal.get(Calendar.MINUTE);
 
                 TimePickerDialog dialog = new TimePickerDialog(AddEvent.this,
                         android.R.style.Theme_Black,
                         (TimePickerDialog.OnTimeSetListener) mTimeSetListener,
-                        hour, minute, true);
+                        hour[0], minute[0], true);
 
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
@@ -99,11 +129,19 @@ public class AddEvent extends AppCompatActivity {
                 Log.d(TAG, "onTimeSet : hh:mm: " + hour + ":" + minute);
 
                 String time = hour + ":" + minute;
-                String timeTest = mDisplaTime.getText().toString().trim();
-                mDisplaTime.setText("Time: " + time);
+                String timeTest = mDisplayTime.getText().toString().trim();
+                mDisplayTime.setText(time);
+
+                cal.set(Calendar.HOUR_OF_DAY, hour);
+                cal.set(Calendar.MINUTE, minute);
+
+                Log.d(TAG, "cal.set(time): " +cal.getTime());
+                Log.e(TAG, "cal.set(time): " +cal.getTimeInMillis());
             }
         };
     }
+
+
 
     public void addEvent() {
 
@@ -125,6 +163,9 @@ public class AddEvent extends AppCompatActivity {
                 database = AppDatabase.getInstance(AddEvent.this);
                 eventList = database.eventDao().getAll();
 
+                Log.d(TAG, "current.set(time): " +current.getTime());
+                Log.e(TAG, "current.set(time): " +current.getTimeInMillis());
+
                 if (!sDescription.equals("") && !sTime.equals("") && !sDate.equals("")) {
                     Event event = new Event();
                     event.setDescription(sDescription);
@@ -136,8 +177,39 @@ public class AddEvent extends AppCompatActivity {
                     editText.setText("");
                     date.setText("");
                     time.setText("");
+
+                    if (cal.getTimeInMillis() > current.getTimeInMillis()) {
+                        Log.e(TAG, "cal > current");
+                        Log.e(TAG, "time" + sDate + sTime);
+                        setAlarm(sDescription, sDate, sTime);
+                    }
+
                 }
             }
         });
     }
+
+    private void setAlarm(String text, String date, String time) {
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(getApplicationContext(), AlarmBrodcast.class);
+        intent.putExtra("event", text);
+        intent.putExtra("time", date);
+        intent.putExtra("date", time);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        String dateandtime = date + " " + time;
+        DateFormat formatter = new SimpleDateFormat("d-M-yyyy hh:mm");
+        try {
+            Date date1 = formatter.parse(dateandtime);
+            am.set(AlarmManager.RTC_WAKEUP, date1.getTime(), pendingIntent);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        finish();
+
+    }
+
 }
